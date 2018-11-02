8afa30081126f5da64424ebabce4d9563d91dbfa
WareJosephB <Ware.JosephB@Gmail.com>
2018-11-02 11:37:06 +0000

    Test to ensure classroom chaining works- passes

--- src/test/java/Outputtest.java
+++ src/test/java/Outputtest.java
@@ -18,19 +18,30 @@ public class Outputtest {
 		Trainee Joe = new Trainee("Joe Bloggs");
 		Trainee Jane = new Trainee("Jane Bloggs");
 		
+		Trainer Matt = new Trainer("Matt Hunt");
+		Trainee David = new Trainee("David Bloggs");
+		Trainee Ceri = new Trainee("Ceri Bloggs");
+		
+		List<Trainee> room2Trainees = new ArrayList<Trainee>();
+		room2Trainees.add(David);
+		room2Trainees.add(Ceri);
+		
 		List<Trainee> Trainees = new ArrayList<Trainee>();
 		Trainees.add(Joe);
 		Trainees.add(Jane);
 		
 		Classroom thisRoom = new Classroom(John, Trainees);
+		Classroom thatRoom = new Classroom(Matt, room2Trainees);
+		
+		List<Classroom> School = new ArrayList<Classroom>();
+		School.add(thisRoom);
+		School.add(thatRoom);
 		
 		assertEquals(
-				"[{\"classroomID\": 1,\"trainer\": \"John Gordon\",\"trainees\": [{	\"traineeID\": 1, \"traineeName\": \"Joe Bloggs\"},{\"traineeID\": 2, \"traineeName\": \"Jane Bloggs\"}]]",
-				JSONTools.JSONfromObject(thisRoom));
+				"[{\"id\":0,\"roomLead\":{\"id\":0,\"name\":\"John Gordon\"},\"students\":[{\"name\":\"Joe Bloggs\",\"technicalSkills\":0,\"softSkills\":0,\"id\":0,\"classRoomid\":0},{\"name\":\"Jane Bloggs\",\"technicalSkills\":0,\"softSkills\":0,\"id\":0,\"classRoomid\":0}]},{\"classroomID\": 2,\"trainer\": \"Matt Hunt\",\"trainees\":[{\"traineeID\":3,\"traineeName\":\"David Bloggs\"},{\"traineeID\":4,\"traineeName\":\"Ceri Bloggs\"}]}]"
+				, JSONTools.JSONfromObject(School));
 
 	}
 
 }
 
-
-//,{\"classroomID\": 2,\"trainer\": \"Matt Hunt\",\"trainees\":[{\"traineeID\":3,\"traineeName\":\"David Bloggs\"},{\"traineeID\":4,\"traineeName\":\"Ceri Bloggs\"}]}
